c869c7f8fd0ec57f927f81f40f4129ca5d02024d
Shinevenbrighter <147421486+Shinevenbrighter@users.noreply.github.com>
2026-03-18 22:18:38 +0000

    Deploying to gh-pages from @ jvalverr/jvalverr.github.io@0f1e2fcdf28b426a9401add04f13bc165f60d0d7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jvalverr/jvalverr",
+    "data-repo": "jvalverr/jvalverr.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
